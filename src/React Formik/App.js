4c5529618b9aba183f323401aba437e90b25bbd9
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-01-18 12:16:18 +0530

    RF Handling SUbmissions

--- src/React Formik/App.js
+++ src/React Formik/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Form from "./Simple form/Form";
-import Formikform from "./FormikHook(useFormik)/Form";
+import Formikform from "./FormikHook(useFormik)(Managing from state)/Form";
+import HandleSubmission from "./Handling submissions/Form";
 import "../App.css";
 
 const App = () => {
   return (
     <div className="App">
       {/* <Form /> */}
-      <Formikform />
+      {/* <Formikform /> */}
+      <HandleSubmission />
     </div>
   );
 };
---
+++ src/React Formik/Handling submissions/Form.js
@@ -0,0 +1,54 @@
+import { useFormik } from "formik";
+import React from "react";
+
+const HandleSubmission = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit: (values) => {
+      console.log("Form data", values);
+    },
+  });
+  // console.log("Form Data", formik.values);
+
+  return (
+    <div>
+      <h1>Simple Form</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+
+        <label htmlFor="email">E-Mail</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default HandleSubmission;
