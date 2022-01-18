93b24d27bd27c4def544b48c0e720c25808bb431
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-01-18 13:29:12 +0530

    RF validation using Yup library

--- src/React Formik/Validations/Validation.js
+++ src/React Formik/Validations/Validation.js
@@ -1,5 +1,6 @@
 import { useFormik } from "formik";
 import React from "react";
+import * as Yup from "yup";
 
 const initialValues = {
   name: "",
@@ -10,30 +11,39 @@ const onSubmit = (values) => {
   console.log("Form data", values);
 };
 
-const validate = (values) => {
-  let errors = {};
-  if (!values.name) {
-    errors.name = "Required";
-  }
-  if (!values.email) {
-    errors.email = "Required";
-  }
-  //  else if (
-  //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-  //   ) {
-  //     errors.email = "Invalid email format";
-  //   }
-  if (!values.channel) {
-    errors.channel = "Required";
-  }
-  return errors;
-};
+//Validation using Yup library**********
+const validationSchema = Yup.object({
+  name: Yup.string().required("Required!"),
+  email: Yup.string().email("Invalid Format").required("Required!"),
+  channel: Yup.string().required("Required!"),
+});
+
+// Custom Validation *************
+// const validate = (values) => {
+//   let errors = {};
+//   if (!values.name) {
+//     errors.name = "Required";
+//   }
+//   if (!values.email) {
+//     errors.email = "Required";
+//   }
+//   //  else if (
+//   //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+//   //   ) {
+//   //     errors.email = "Invalid email format";
+//   //   }
+//   if (!values.channel) {
+//     errors.channel = "Required";
+//   }
+//   return errors;
+// };
 
 const Validations = () => {
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate,
+    // validate,
+    validationSchema,
   });
   console.log("Visited Fields", formik.touched);
 
